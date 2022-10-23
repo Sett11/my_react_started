bc2b3a7c452534ff60949d17b2471d464aa46186
Jhonny <gonzodzen@mail.ru>
2022-10-24 02:24:01 +0300

    Next commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = ()=>{
+    return(
+        <div className='content'>
+        <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ_G9U9095poYEIvtg8fnA2Ef3dcjLEebptQ&usqp=CAU"/>
+        </div>
+       <div>
+        ava + description
+       </div>
+       <div>
+        My posts
+        <div>New post
+  
+        </div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+       </div>
+       </div>
+    )
+}
+
+
+
+export default Profile
