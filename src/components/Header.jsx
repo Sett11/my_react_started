4847d4fb1e6f0d5399ee35d1257b5cb55c20cc92
Jhonny <gonzodzen@mail.ru>
2022-10-24 09:15:37 +0300

    next commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ()=>{
     return(
         <header className='header'>
-      <img src='https://sun9-53.userapi.com/impf/c636929/v636929404/2435d/qWXmE4Qjx34.jpg?size=1280x1006&quality=96&sign=21ba7467f75f0aa1c407c32bf216cdf7&type=album'/>
+      <a href='#!'><img src='https://sun9-53.userapi.com/impf/c636929/v636929404/2435d/qWXmE4Qjx34.jpg?size=1280x1006&quality=96&sign=21ba7467f75f0aa1c407c32bf216cdf7&type=album'></img></a>
      </header>
     )
 }
